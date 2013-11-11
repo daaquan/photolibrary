ee60cf4c674087a45edc9a25a15277ff33d82374
Robbert Klarenbeek <robbertkl@renbeek.nl>
2013-11-11 23:18:39 +0100

    Add example to show how to list all events

---
+++ examples/events.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Lists all events in your iPhoto library, together with the number of photos, sorted alphabetically
+ *
+ * Usage: php examples/events.php [path-to-iphoto-library]
+ *
+ * @author Robbert Klarenbeek <robbertkl@renbeek.nl>
+ * @copyright Copyright (c) 2013, Robbert Klarenbeek
+ * @package photolibrary
+ */
+
+error_reporting(-1);
+ini_set('display_errors', 'on');
+
+// Change this if you're not using Composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// Default to OS X default iPhoto library
+$libraryPath = ($argc >= 2) ? $argv[1] : $_SERVER['HOME'] . '/Pictures/iPhoto Library.photolibrary';
+
+$library = new \PhotoLibrary\Library($libraryPath);
+
+$events = array();
+foreach ($library->getAlbumsOfType('Event') as $album) {
+    $events[$album->getName()] = $album->getPhotoCount();
+}
+
+ksort($events);
+foreach ($events as $eventName => $photoCount) {
+    echo $eventName . ' (' . $photoCount . ' photos)' . PHP_EOL;
+}
