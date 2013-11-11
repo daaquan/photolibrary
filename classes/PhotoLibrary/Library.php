d9035cb918d1b23941cdb1537303c6ebce5f3802
Robbert Klarenbeek <robbertkl@renbeek.nl>
2013-11-11 22:34:20 +0100

    Add basic entities: Library, Album and Photo

---
+++ classes/PhotoLibrary/Library.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * File containing the \PhotoLibrary\Library class
+ *
+ * @author Robbert Klarenbeek <robbertkl@renbeek.nl>
+ * @copyright Copyright (c) 2013, Robbert Klarenbeek
+ * @package photolibrary
+ */
+
+namespace PhotoLibrary;
+
+/**
+ * Main class for accessing an iPhoto .photolibrary package
+ * @package photolibrary
+ */
+class Library
+{
+    /**
+     * Path to the library on this filesystem
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * The plist data from AlbumData.xml as array
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * Associative array (int => Album) of cached Album objects by ID
+     * @var array
+     */
+    protected $albums = null;
+
+    /**
+     * Create new Library from a path
+     * @param string $path path to the .photolibrary directory (with or without trailing /)
+     */
+    public function __construct($path)
+    {
+        $path = rtrim($path, '/');
+        $plist = $path . '/AlbumData.xml';
+        
+        if (!is_dir($path) || !file_exists($plist)) {
+            throw new \InvalidArgumentException('Given path does not seem to be an iPhoto library package');
+        }
+
+        $this->path = $path;
+
+        $plist = new \CFPropertyList\CFPropertyList($plist);
+        $this->data = $plist->toArray();
+    }
+
+    /**
+     * Ensure the album cache ($this->albums) is filled with albums of this library
+     */
+    protected function ensureAlbums()
+    {
+        if (is_null($this->albums)) {
+            $this->albums = array();
+            foreach ($this->data['List of Albums'] as &$albumData) {
+                $album = new Album($this, $albumData);
+                $this->albums[$album->getId()] = $album;
+            }
+        }
+    }
+
+    /**
+     * Get all albums in this library
+     * @return Album[] list of Album objects
+     */
+    public function getAlbums()
+    {
+        $this->ensureAlbums();
+        return array_values($this->albums);
+    }
+
+    /**
+     * Get all albums of a specific "Album Type"
+     * @param string $type album type to look for, e.g. "Flagged", "Regular", "Event"
+     * @return Album[] list of Album objects
+     */
+    public function getAlbumsOfType($type)
+    {
+        $this->ensureAlbums();
+        $albums = array();
+        foreach ($this->albums as $id => $album) {
+            if ($album->getType() == $type) {
+                $albums[] = $album;
+            }
+        }
+        return $albums;
+    }
+
+    /**
+     * Get an album by its "Album ID"
+     * @param int $id Album ID of the album to get
+     * @return Album album with the given ID, or null iff not found
+     */
+    public function getAlbum($id)
+    {
+        $this->ensureAlbums();
+        if (!array_key_exists($id, $this->albums)) {
+            return null;
+        }
+        return $this->albums[$id];
+    }
+
+    /**
+     * Get a photo by its key
+     * @param int $key key of the photo to get
+     * @return Photo photo with the given key, or null iff not found
+     */
+    public function getPhoto($key)
+    {
+        if (!array_key_exists($key, $this->data['Master Image List'])) {
+            return null;
+        }
+        return new Photo($this, $key, $this->data['Master Image List'][$key]);
+    }
+
+    /**
+     * Rewrite an internal photolibrary path (called the "Archive Path" to the real one on disk
+     * @param string $path path to a file from the AlbumData.xml plist
+     * @return string rewritten path to the real file (of the current .photolibrary)
+     */
+    public function rewritePath($path)
+    {
+        $archivePath = dirname($this->data['Archive Path']);
+        $realPath = dirname($this->path);
+        return preg_replace('/^' . preg_quote($archivePath, '/') . '/', $realPath, $path);
+    }
+}
---
+++ classes/PhotoLibrary/Photo.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * File containing the \PhotoLibrary\Photo class
+ *
+ * @author Robbert Klarenbeek <robbertkl@renbeek.nl>
+ * @copyright Copyright (c) 2013, Robbert Klarenbeek
+ * @package photolibrary
+ */
+
+namespace PhotoLibrary;
+
+/**
+ * Class representing a photo within the iPhoto .photolibrary package
+ * @package photolibrary
+ */
+class Photo
+{
+    /**
+     * Link back to the library to which this photo belongs
+     * @var Library
+     */
+    protected $library;
+
+    /**
+     * Key of the photo (a unique identifier within the AlbumData.xml plist)
+     * @var int
+     */
+    protected $key;
+
+    /**
+     * Associative array with raw photo properties
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * Create new Photo by supplying the raw properties
+     * @param Library $library library to which this photo belongs
+     * @param int $key key of the photo (identifier from the AlbumData.xml plist)
+     * @param array $data assiciative array with raw photo properties
+     * @see Library::getPhoto()
+     */
+    public function __construct(Library $library, $key, $data)
+    {
+        $this->library = $library;
+        $this->key = $key;
+        $this->data = $data;
+    }
+
+    /**
+     * Get the key of this photo (a unique identifier within the AlbumData.xml plist)
+     * @return int key of this photo
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * Get the caption of this photo (usually the original filename)
+     * @return string caption of this photo
+     */
+    public function getCaption()
+    {
+        return $this->data['Caption'];
+    }
+
+    /**
+     * Get the path to the (original) image file on disk
+     * @return string path to the image file
+     */
+    public function getPath()
+    {
+        return $this->library->rewritePath($this->data['ImagePath']);
+    }
+
+    /**
+     * Get the path to the iPhoto-generated thumbnail file on disk
+     * @return string path to the thumbnail file
+     */
+    public function getThumbnailPath()
+    {
+        return $this->library->rewritePath($this->data['ThumbPath']);
+    }
+
+    /**
+     * Get the string representation of this photo
+     * @return string string representation of this photo (just it's caption)
+     */
+    public function __toString()
+    {
+        return $this->getCaption();
+    }
+}
