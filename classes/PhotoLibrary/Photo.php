2aaf0565c7fcfc39bbc473e22e6bb6b5c0146abb
Christopher Finke <chris@efinke.com>
2014-07-07 01:54:43 +0000

    Switching getDate to getDateTime and returning a DateTime object.

--- classes/PhotoLibrary/Photo.php
+++ classes/PhotoLibrary/Photo.php
@@ -102,13 +102,13 @@ class Photo
      * @param string $format The date formatting string.
      * @return string The photo date.
      */
-    public function getDate( $format = "Y-m-d H:i:s" )
+    public function getDateTime($format = "Y-m-d H:i:s")
     {
         // iPhoto stores timestamps as offsets from 2001-01-01 00:00:00.
         // gmmktime( 0, 0, 0, 1, 1, 2001 ) = 978307200
         $IPHOTO_EPOCH = 978307200;
 
-        return date( $format, $IPHOTO_EPOCH + intval( $this->data['DateAsTimerInterval'] ) );
+        return new \DateTime('@' . ($IPHOTO_EPOCH + intval($this->data['DateAsTimerInterval'])));
     }
 
     /**
